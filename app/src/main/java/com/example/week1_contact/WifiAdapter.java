7f1a2c15b0906f971e707c7665aebf3d6f3b31ca
guswjd2103 <44867153+guswjd2103@users.noreply.github.com>
2019-12-31 22:55:33 +0900

    remove view holder class in adpater

--- app/src/main/java/com/example/week1_contact/WifiAdapter.java
+++ app/src/main/java/com/example/week1_contact/WifiAdapter.java
@@ -1,19 +1,15 @@
 package com.example.week1_contact;
 
 import android.content.Context;
-import android.net.wifi.ScanResult;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-
-//import com.example.week1_contact.databinding.ItemAccesspointBinding;
+import androidx.annotation.Nullable;
 
 import java.util.List;
-import java.util.Vector;
 
 public class WifiAdapter extends ArrayAdapter<WifiData> {
     Context context;
@@ -31,36 +27,44 @@ public class WifiAdapter extends ArrayAdapter<WifiData> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
+    public View getView(int position, @Nullable View convertView, @Nullable ViewGroup parent) {
+//        ViewHolder holder;
 
-        if(convertView == null) {
-            convertView = inflater.inflate(resource, null);
-            holder = new ViewHolder();
+//        if(convertView == null) {
+            convertView = inflater.inflate(resource, parent, false);
 
-            holder.ssidTextView = (TextView) convertView.findViewById(R.id.ssidTextView);
-            holder.bssidTextView = (TextView) convertView.findViewById(R.id.bssidTextView);
-            holder.rssiLevelTextView = (TextView) convertView.findViewById(R.id.rssiLevelTextView);
+            TextView ssid = (TextView) convertView.findViewById(R.id.ssidTextView);
+            TextView bssid = (TextView) convertView.findViewById(R.id.bssidTextView);
+            TextView rssi = (TextView) convertView.findViewById(R.id.rssiLevelTextView);
 
-            convertView.setTag(holder);
+            ssid.setText(wifiData.get(position).getSSID());
+            bssid.setText(wifiData.get(position).getBSSID());
+            rssi.setText(wifiData.get(position).getRSSI());
+//            holder = new ViewHolder();
+//
+//            holder.ssidTextView = (TextView) convertView.findViewById(R.id.ssidTextView);
+//            holder.bssidTextView = (TextView) convertView.findViewById(R.id.bssidTextView);
+//            holder.rssiLevelTextView = (TextView) convertView.findViewById(R.id.rssiLevelTextView);
+
+//            convertView.setTag(holder);
 
 
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
+//        } else {
+//            holder = (ViewHolder) convertView.getTag();
+//        }
 
-        WifiData wifiDatas = wifiData.get(position);
+//        WifiData wifiDatas = wifiData.get(position);
 
-        holder.ssidTextView.setText(wifiDatas.getSSID());
-        holder.bssidTextView.setText(wifiDatas.getBSSID());
-        holder.rssiLevelTextView.setText(wifiDatas.getRSSI());
+//        holder.ssidTextView.setText(wifiData.get(position).getSSID());
+//        holder.bssidTextView.setText(wifiData.get(position).getBSSID());
+//        holder.rssiLevelTextView.setText(wifiData.get(position).getRSSI());
 
         return convertView;
     }
 
-    class ViewHolder {
-        TextView ssidTextView;
-        TextView bssidTextView;
-        TextView rssiLevelTextView;
-    }
+//    class ViewHolder {
+//        TextView ssidTextView;
+//        TextView bssidTextView;
+//        TextView rssiLevelTextView;
+//    }
 }
